f48f2a83fe256993361d0f718aca29ea9acf2842
PichayesT <Pichayes_Thimpia@outlook.com>
2025-05-12 03:08:22 +1000

    add

--- e-commerce/View/product.php
+++ e-commerce/View/product.php
@@ -56,7 +56,12 @@
 
                 // Fetch the products from the database
                 $sql = "SELECT * FROM lpa_stock WHERE lpa_stock_status = '1'";  // Adjust this query to match your table and columns
+                
+                $start_time = microtime(true); // Add
                 $result = $conn->query($sql);
+                $end_time = microtime(true); // Add
+                $execution_time = $end_time - $start_time; //Add
+                error_log("Product fetch execution time: $execution_time seconds"); // Add
 
                 if ($result->num_rows > 0) {
                     // Output each product
